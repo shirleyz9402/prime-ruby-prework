9319f1b6b40a0fde4d10d6fccb9c06841f11cbe6
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-21 21:44:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,12 @@
 def prime?(number)
   divider = Array(2..number - 1)
+  i = 0
+  arr = []
   loop do 
   if number < 0
     return false
-  elsif
-    number % divider
+  elsif number % divider[i] == 0
+    return false
+  elsif number % divider[i] > 0
+    i++
  end
